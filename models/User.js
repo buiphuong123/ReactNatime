ed08b8e26fbac2ee16043d9ba81557d5da7fef32
unknown <buiphuong>
2021-05-31 20:57:19 +0700

    code

--- models/User.js
+++ models/User.js
@@ -7,11 +7,14 @@ const userSchema = mongoose.Schema({
         type: String,
         require: true,
     },
-    // tuong tu voi cac truong du lieu con lai :))
+    email: {
+        type: String,
+        require: true,
+    },
     password: {
         type: String,//ma hoa pass the nao ti nua 2 cais thoi it the :))lm mau 
         require: true,
     },
-});//tiep di 
+}); 
 
 module.exports = mongoose.model('User', userSchema); // the tom lai day laf bang user dugn roiok
--- routes/authentication/register.js
+++ routes/authentication/register.js
@@ -6,13 +6,16 @@ const bcrypt = require('bcryptjs');
 const register = express.Router();
 
 register.post('/register', async (req, res) => {
-    const {username, password} = req.body;
+    const {username, email, password} = req.body;
+    console.log(req.body);
     // kiem tra xem user cho trong db chua
     try {
         const user = await User.findOne({username});
         if (user) return res.json({error: 'Tai khoan da ton tai'});
+        const usermail = await User.findOne({email});
+        if(usermail) return res.json({error: 'Email da duoc su dung'});
         const hashedPassword = await bcrypt.hash(password, 12);
-        const newUser = new User({username, password: hashedPassword});
+        const newUser = new User({username, email, password: hashedPassword});
         await newUser.save();
         return res.json({message: 'Tao tai khoan thanh cong', user: newUser});
     } catch (error) {
