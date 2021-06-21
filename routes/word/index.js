4b3939dc38058ccb903b31163a914e3578336e50
unknown <buiphuong>
2021-06-21 10:19:56 +0700

    update

--- routes/word/index.js
+++ routes/word/index.js
@@ -3,5 +3,4 @@ const getword = require('./getWord');
 const findUserLike = require('./findUserLike');
 const findUserMemerize = require('./findUserMemerize');
 const listWordNotMemerize = require('./listWordNotMemerize');
-const listWordLikeMem = require('./listWordLikeMem');
-module.exports = {createWord, getword, findUserLike, findUserMemerize, listWordNotMemerize, listWordLikeMem};
+module.exports = {createWord, getword, findUserLike, findUserMemerize, listWordNotMemerize};
